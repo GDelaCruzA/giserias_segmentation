93670d52cf38d5fe2a2d1677ad4fe81441893589
GDelaCruzA <gaguero@ipn.mx>
2024-02-13 14:59:31 -0700

    primera aproxmación a los indices y segmentación de imagenes

---
+++ indices_vegetacion_RGB.R
@@ -0,0 +1,60 @@
+# funciones con los índices de vegetación con base RGB
+# G. De la Cruz A. @ 20240213
+# 
+# Jiang, j. et al. 2019. Using Digital Cameras on an Unmanned Aerial Vehicle to
+# Derive Optimum Color Vegetation Indices for Leaf Nitrogen Concentration
+# Monitoring in Winter Wheat. Remote Sensing, 11, 2667; doi:10.3390/rs11222667
+#
+# Normalized green red difference index
+NGRDI <- function(img, r = 1, g = 2, b = 3) {
+  rband <- img[[r]]
+  gband <- img[[g]]
+  bband <- img[[b]]
+  indice <- (gband-rband) / (gband + rband)
+  return(indice)
+}
+
+# Kawashima index
+KI <- function(img, r = 1, g = 2, b = 3) {
+  rband <- img[[r]]
+  gband <- img[[g]]
+  bband <- img[[b]]
+  indice <- (rband-bband) / (rband + bband)
+  return(indice)
+}
+
+# Red green ratio index
+RGRI <- function(img, r = 1, g = 2, b = 3) {
+  rband <- img[[r]]
+  gband <- img[[g]]
+  bband <- img[[b]]
+  indice <- rband / gband
+  return(indice)
+}
+
+# Visible atmospherically resistance index
+RGRI <- function(img, r = 1, g = 2, b = 3) {
+  rband <- img[[r]]
+  gband <- img[[g]]
+  bband <- img[[b]]
+  indice <- rband / gband
+  return(indice)
+}
+
+# Excess green vegetation index
+EGVI <- function(img, r = 1, g = 2, b = 3) {
+  rband <- img[[r]]
+  gband <- img[[g]]
+  bband <- img[[b]]
+  indice <- (2 * gband - rband - bband) / (gband + rband + bband)
+  return(indice)
+}
+
+# True Color Vegetation Index
+TCVI <- function(img, r = 1, g = 2, b = 3, ajuste = 0.4) {
+  rband <- img[[r]]
+  gband <- img[[g]]
+  bband <- img[[b]]
+  indice <- 1.4 * (2 * rband - 2 * bband) / (2 * rband - gband - 2 * bband + 255 * ajuste)
+  return(indice)
+}
---
+++ segmentation02_terra.R
@@ -0,0 +1,39 @@
+#prueba de segmentación paquetes actualizados
+#
+library(terra) |> suppressPackageStartupMessages()
+library(OpenImageR) |> suppressPackageStartupMessages()
+
+source(file = "indices_vegetacion_RGB.R")
+
+# Thanks to Matthias Forkel we can use a nice palette for vegetation indices:
+colores <- function(n) {
+  .fun <- colorRampPalette(c("chocolate4", "orange", "yellow", "grey", "green", "green3", "darkgreen"))
+  col <- .fun(n)
+  return(col)
+}
+
+# se lee la imagen
+r = rast("~/giserias/conchalito-vuelo2/RPubs_crops.jpg")
+r = rast("~/giserias/conchalito-vuelo2/conchal_10.tif")
+
+# Se definen las bandas que se interpretarían como RGB y se visualiza
+RGB(r) <- 1:3
+plot(r) 
+
+# se calculan los índices para la imagen
+r_egvi <- EGVI(r)
+plot(r_egvi, main = "ExG Vegetation Index sin rampa")
+
+# se definen los intervalos y colores para la imagen
+cortes <- seq(-0.35, 1, by=0.1)
+cols <- colores(length(cortes)-1)
+
+plot(r_egvi, col = cols, breaks=cortes, main = "ExG Vegetation Index con rampa")
+
+r_tcvi <- TCVI(r)
+plot(r_tcvi, col = cols, breaks=cortes, main = "ExG Vegetation Index con rampa")
+
+# probar con los otros índices y rampas de color
+
+## Segmentación de imágenes
+
